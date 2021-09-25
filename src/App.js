8d12acb1b63ff76bc0b9749ee785b891ca7f7797
Neo <neocamanga@knights.ucf.edu>
2021-09-25 19:48:12 -0400

    Time input; month display

--- src/App.js
+++ src/App.js
@@ -20,17 +20,7 @@ function App(props) {
     return (
         <div className="App">
             <header className="App-header">
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+            
                 <p>{props.text}</p>
                 {/* <p>{item}</p> */}
                 <Calendar />
--- src/Calendar.js
+++ src/Calendar.js
@@ -1,11 +1,12 @@
 import {
-  addDays,
-  differenceInCalendarDays,
-  isSameDay,
-  endOfMonth,
-  endOfWeek,
-  startOfMonth,
-  startOfWeek,
+    addDays,
+    differenceInCalendarDays,
+    isSameDay,
+    endOfMonth,
+    endOfWeek,
+    startOfMonth,
+    startOfWeek,
+    getMonth,
 } from "date-fns";
 import "./index.css";
 import { EventObject } from "./EventObject";
@@ -14,120 +15,165 @@ import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import TimePicker from 'react-time-picker';
+import format from "date-fns/format/index";
 
 const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  pt: 2,
-  px: 4,
-  pb: 3,
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    pt: 2,
+    px: 4,
+    pb: 3,
 };
 
 const Calendar = () => {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => {
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setOpen(false);
-  };
+    const [open, setOpen] = React.useState(false);
+    const [value, onChange] = useState(new Date());
 
-  let day = startOfWeek(startOfMonth(new Date()));
-  const lastCalendarDay = endOfWeek(endOfMonth(new Date()));
-  const endDay = addDays(lastCalendarDay, 1); // This is to create an exclusive end date
-  const totalDays = differenceInCalendarDays(lastCalendarDay, day) + 1; // +1 to add end date
-  console.log(day, lastCalendarDay);
-  console.log(totalDays);
+    const handleOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
 
-  const calendar = [];
-  let week = [];
-  var eventArr = [];
+    const today = new Date();
+    let day = startOfWeek(startOfMonth(today));
+    const lastCalendarDay = endOfWeek(endOfMonth(today));
+    const endDay = addDays(lastCalendarDay, 1); // This is to create an exclusive end date
+    const totalDays = differenceInCalendarDays(lastCalendarDay, day) + 1; // +1 to add end date
+    const monthNumber = getMonth(today);
+    const monthName = format(new Date(2000, monthNumber, 1), 'MMMM',)
+    console.log(day, lastCalendarDay);
+    console.log(totalDays);
 
-  eventArr.push(new EventObject("h", "h", "h", 3));
-  eventArr.push(new EventObject("h", "h", "h", 17));
+    const calendar = [];
+    let week = [];
+    var eventArr = [];
 
-  console.log("event is " + JSON.stringify(eventArr));
+    eventArr.push(new EventObject("h", "h", "h", 3));
+    eventArr.push(new EventObject("h", "h", "h", 17));
 
-  for (let counter = 1; !isSameDay(day, endDay); counter++) {
-    week.push(day);
-    day = addDays(day, 1);
+    console.log("event is " + JSON.stringify(eventArr));
 
-    if (counter === 7) {
-      calendar.push(week);
-      week = [];
-      counter = 0;
+    for (let counter = 1; !isSameDay(day, endDay); counter++) {
+        week.push(day);
+        day = addDays(day, 1);
+
+        if (counter === 7) {
+            calendar.push(week);
+            week = [];
+            counter = 0;
+        }
     }
-  }
 
-  console.log(calendar);
+    console.log(calendar);
 
-  function addEventListener(number) {
-    console.log(number);
-  }
+    function addEventListener(number) {
+        console.log(number);
+    }
 
-  return (
-    <div className="calendar">
-      <div className="tab">
-        {/* <DaysOfTheWeek /> */}
-        Sun &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; Mon &nbsp; &nbsp; &nbsp;
-        &nbsp; &nbsp;&nbsp;Tues &nbsp; &nbsp; &nbsp; &nbsp; Weds &nbsp; &nbsp;
-        &nbsp; &nbsp; Thurs &nbsp; &nbsp; &nbsp; &nbsp; Fri &nbsp; &nbsp; &nbsp;
-        &nbsp; Sat &nbsp; &nbsp; &nbsp; &nbsp; Sun
-      </div>
-      {calendar.map((week) => {
-        return (
-          <div key={week[0]} className="weeks">
-            {week.map((day) => {
-              return (
-                <div key={day.getDate()} className="days" onClick={handleOpen}>
-                  {day.getDate()}
-                  {eventArr.map((post) => {
-                    if (post.time === day.getDate())
-                      return <h1>{post.title}</h1>;
-                  })}
-                </div>
-              );
-            })}
-          </div>
-        );
-      })}
+    function printStuff() { }
 
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <form>
-            <p>event title</p>
-            <input type="text" />
+    return (
+        <React.Fragment>
+            {monthName}
+            <DaysOfTheWeek />
+            <div className="calendar">
+                {calendar.map((week) => {
+                    return (
+                        <div key={week[0]} className="weeks">
+                            {week.map((day) => {
+                                return (
+                                    <div
+                                        key={day.getDate()}
+                                        className="days"
+                                        onClick={handleOpen}
+                                    >
+                                        {day.getDate()}
+                                        {eventArr.map((post) => {
+                                            if (post.time === day.getDate())
+                                                return <h1>{post.title}</h1>;
+                                        })}
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    );
+                })}
 
-            <p>event date</p>
-            <input type="date" />
-          </form>
-        </Box>
-      </Modal>
-    </div>
-  );
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <Box sx={style}>
+                        <form>
+                            <p>Event title</p>
+                            <input type="text" />
+
+                            <p>Event description</p>
+                            <input type="text" />
+
+                            <p>Event address</p>
+                            <input type="text" />
+
+                            <p>Event start time</p>
+                            <TimePicker disableClock="true" onChange={onChange} value={value} />
+
+                            <p>Event start end</p>
+
+                            <Button variant="outlined" onClick={() => printStuff()}>
+                                Click me hehe
+                            </Button>
+                        </form>
+                    </Box>
+                </Modal>
+            </div>
+        </React.Fragment>
+    );
 };
 
 const DaysOfTheWeek = () => {
-  const DoTW = ["Sun", "Mon", "Tues", "Weds", "Thurs", "Fri", "Sat", "Sun"];
+    const DoTW = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
 
-  return (
-    <div className="weeks">
-      {DoTW.map((day) => {
-        return day;
-      })}
-    </div>
-  );
+    return (
+        <div className="">
+            {DoTW.map((day) => {
+                return (
+                    <div key={day} className="DoTW">
+                        {day}
+                    </div>
+                );
+            })}
+        </div>
+    );
+    // return (
+    //     <div className="dayList">
+    //         {DoTW.map((day) => {
+    //             return (
+    //                 <div className="dayName">
+    //                   {day}
+    //                 </div>
+    //             )
+    //         })}
+    //     </div>
+    // );
 };
 
 export default Calendar;

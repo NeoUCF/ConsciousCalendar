db09329870bf9d50357fcb2014672786ab9e0995
annsian <annmai1202@gmail.com>
2021-09-25 21:57:45 -0400

    added images and styles

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,13 +1,20 @@
 const Homepage = () => {
   return (
     <div>
-      <div></div>
+      <div><h1 id="site-title">Conscious Calendar</h1></div>
 
       <div>
-      <img src="image/road.png" alt="apple"/></div>
+        <img id="road-image" src="image/road.png" alt="road" />
+        <img id="calendar-image" src="image/calendar.png" alt="calendar" />
+        <img id="sunny-image" src="image/sunny.png" alt="sunny" />
+        <img id="weather-image" src="image/weather.png" alt="weather" />
+
+      </div>
+
+
     </div>
 
-  
+
   );
 };
 
